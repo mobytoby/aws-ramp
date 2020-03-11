1c357a05da2613b1b4ef42b9ffbb3fe59484c255
Toby Buckley <mobytoby@gmail.com>
2020-03-11 14:32:23 -0700

    finished crud operations in photo storage

--- photo-gallery/apps/photo-storage/main.go
+++ photo-gallery/apps/photo-storage/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
-	"log"
 	"mime/multipart"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -26,30 +27,71 @@ func main() {
 
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.POST("/upload/:bucketId", func(c *gin.Context) {
+	router.POST("/bucket/:bucketId/photos/:photoName", func(c *gin.Context) {
 		// single file
 		bucketID := c.Param("bucketId")
+		photoName := c.Param("photoName")
 		file, err := c.FormFile("file")
 		check(err)
-		log.Println(file.Filename, bucketID)
 		sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2")})
-		res, err := uploadFile(sess, file, bucketID)
+		res, err := uploadFile(sess, file, bucketID, photoName)
 		check(err)
 		c.JSON(http.StatusOK, res)
+	})
+
+	router.GET("/bucket/:bucketId/photos", func(c *gin.Context) {
+		bucket := c.Param("bucketId")
+		svc := s3.New(
+			session.Must(session.NewSession()),
+			aws.NewConfig().WithRegion("us-west-2"),
+		)
+		res, err := svc.ListObjects(&s3.ListObjectsInput{
+			Bucket: aws.String(bucket),
+		})
+		check(err)
+		urls := make([]string, len(res.Contents))
+		check(err)
+		for i := 0; i < len(res.Contents); i++ {
+			key := res.Contents[i].Key
+			urlStr, err := getPresignedURL(svc, bucket, key)
+			check(err)
+			urls[i] = urlStr
+		}
+		c.JSON(http.StatusOK, urls)
+	})
+
+	router.GET("/bucket/:bucketId/photos/:photoName", func(c *gin.Context) {
+		bucket := c.Param("bucketId")
+		key := c.Param("photoName")
+		svc := s3.New(
+			session.Must(session.NewSession()),
+			aws.NewConfig().WithRegion("us-west-2"),
+		)
+		url, err := getPresignedURL(svc, bucket, &key)
+		check(err)
+		c.JSON(http.StatusOK, url)
 	})
 
 	router.Run(":8080")
 }
 
 func check(e error) {
+	fmt.Println(e)
 	if e != nil {
 		panic(e)
 	}
 }
 
-func uploadFile(session *session.Session, file *multipart.FileHeader, bucket string) (*s3.PutObjectOutput, error) {
-	f, err := file.Open()
+func getPresignedURL(svc *s3.S3, bucket string, key *string) (string, error) {
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    key,
+	})
+	return req.Presign(15 * time.Minute)
+}
 
+func uploadFile(session *session.Session, file *multipart.FileHeader, bucket string, photoName string) (*s3.PutObjectOutput, error) {
+	f, err := file.Open()
 	defer f.Close()
 	check(err)
 	buf := bytes.NewBuffer(nil)
@@ -58,7 +100,7 @@ func uploadFile(session *session.Session, file *multipart.FileHeader, bucket str
 
 	return s3.New(session).PutObject(&s3.PutObjectInput{
 		Bucket:               aws.String(bucket),
-		Key:                  aws.String(file.Filename),
+		Key:                  aws.String(photoName),
 		ACL:                  aws.String("private"),
 		Body:                 bytes.NewReader(buf.Bytes()),
 		ContentLength:        aws.Int64(file.Size),
