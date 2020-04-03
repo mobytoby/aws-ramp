56dbb9aadaa05c1f9158e1aaf04ff5ebf91ca24c
Toby Buckley <mobytoby@gmail.com>
2020-04-03 09:34:00 -0700

    Pulling params from SSM and sending ECS tasks from Lambda

---
+++ photo-gallery/apps/job-scheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace job_scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Got this: {args}\n");
+        }
+    }
+}
--- photo-gallery/lambda/ProcessDynamoDbRecords/src/index.ts
+++ photo-gallery/lambda/ProcessDynamoDbRecords/src/index.ts
@@ -3,11 +3,13 @@ import "reflect-metadata";
 import { DynamoDBStreamEvent } from "aws-lambda";
 import { fromDb, Attributes } from "@shiftcoders/dynamo-easy";
 import { ImageJob } from "./imagejob.model";
-import SQS from "aws-sdk/clients/sqs";
-
+import { default as ECS } from "aws-sdk/clients/ecs";
+import { default as SSM, GetParametersResult } from "aws-sdk/clients/ssm";
+import { waterfall } from "async";
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
-  const sqs = new SQS();
+  const ecs = new ECS({ region: 'us-west-2' });
+  const ssm = new SSM({ region: 'us-west-2' });
   const allPromises: Promise<any>[] = event.Records
   .filter(record => record.eventName === 'INSERT')
   .map(record => {
@@ -16,18 +18,69 @@ export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
       if (!newImage) { return ""; }
       const job = fromDb(<Attributes>newImage, ImageJob);
       const jJob = JSON.stringify(job);
-      sqs.sendMessage({
-          MessageBody: jJob,
-          QueueUrl: 'https://sqs.us-west-2.amazonaws.com/540983619545/imagejobs'
-        }, (err, data) => {
-          if (err) {
-            console.log(`Error sending sqs message. Err: ${err}`);
-            reject(err);
-          }
-          console.log(`Successfully sent sqs message: ${data}`);
-          resolve(data);
-      });
+      const params = [
+        '/photo-gallery/cluster',
+        '/photo-gallery/taskDefinition',
+        '/photo-gallery/subnets',
+        '/photo-gallery/securityGroups',
+      ];
+
+
+      waterfall([
+        function(callback: any) {
+          ssm.getParameters({ Names: params }, 
+            (err: any, data: GetParametersResult) => callback(err, data.Parameters))
+        },
+        function(parameters: any, callback: any) {
+          ecs.runTask({
+            cluster: parameters.find((d: any) => d.Name == '/photo-gallery/cluster').Value,
+            launchType: "FARGATE",
+            taskDefinition: parameters.find((d: any) => d.Name == '/photo-gallery/taskDefinition').Value,
+            networkConfiguration: {
+              awsvpcConfiguration: {
+                subnets: parameters.find((d: any) => d.Name == '/photo-gallery/subnets').Value.split(','),
+                assignPublicIp: "DISABLED",
+                securityGroups: parameters.find((d: any) => d.Name == '/photo-gallery/securityGroups').Value.split(','),
+              }
+            },
+            overrides: {
+              containerOverrides: [
+                { 
+                  name: "job-scheduler",
+                  command: [jJob]
+                }
+              ]
+            }
+          }, (err, data) => { callback(err, data) });
+        }
+      ], function (err, result){
+        if (err) { reject(err); }
+        else resolve(result);
+      });      
     });
   });
   return Promise.all(allPromises);
 }
+
+
+// class Config {
+//   ssm = new SSM({ region: 'us-west-2'});
+//   cluster = await this.GetScalarParameter('photo-gallery/cluster');
+//   // public get Cluster() {
+//   //   this.ssm.getParameter({
+//   //     'photo-gallery/cluster', (err: any, data: any) => {
+//   //     if(err) { return err; }
+//   //     else { return data; }
+//   //     )
+//   //   });
+//   // }
+//   private async GetScalarParameter(key: string) {
+//     return new Promise<string|AWSError>((resolve, reject) => {
+//       this.ssm.getParameter({Name: key},
+//         (err, data) => {
+//           if (err) { reject(err); }
+//           else { resolve(data.Parameter?.Value); }
+//         });
+//     });
+//   }
+// }
