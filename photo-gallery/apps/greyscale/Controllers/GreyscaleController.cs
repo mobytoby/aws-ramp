f61739454c894dda60612e390427d3e4d6d208b7
Toby Buckley <mobytoby@gmail.com>
2020-04-14 12:12:44 -0700

    Cleaned up

--- photo-gallery/apps/greyscale/Controllers/GreyscaleController.cs
+++ photo-gallery/apps/greyscale/Controllers/GreyscaleController.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.Formats.Jpeg;
-using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 
 namespace Greyscale.Controllers
 {
-    [ApiController]
+  [ApiController]
     [Route("/")]
     public class GreyscaleController : ControllerBase
     {
-        private readonly ILogger<GreyscaleController> _logger;
+        private ILogger<GreyscaleController> Logger { get; }
 
         public GreyscaleController(ILogger<GreyscaleController> logger)
         {
-            _logger = logger;
+            Logger = logger;
         }
 
         [HttpGet]
         public string Get()
         {
-            return "Welcome to the greyscale API";
+            return "Welcome to the greyscale API2";
         }
 
         [HttpGet]
@@ -40,14 +35,16 @@ namespace Greyscale.Controllers
         [HttpPost]
         public async Task<IActionResult> Post()
         {
+            Logger.LogInformation($"Received {Request.ContentLength} bytes.");
             using (var inputStream = new MemoryStream())
             using (var outputStream = new MemoryStream())
             {
                 await Request.Body.CopyToAsync(inputStream);
                 var bytes = inputStream.ToArray();
                 var image = Image.Load(bytes, out IImageFormat format);
-                image.Mutate(x => x.Grayscale());
+                image.Mutate(ctx => ctx.Grayscale());
                 image.Save(outputStream, format);
+                Logger.LogInformation($"Transformed. Returning {outputStream.Length} bytes.");
                 return File(outputStream.ToArray(), format.MimeTypes.First());
             }
         }
